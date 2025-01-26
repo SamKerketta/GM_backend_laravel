4f3917f5f4e233146c05c0b3a88248546ad85bf1
sam kerketta <samkerketta77@gmail.com>
2025-01-26 14:45:36 +0530

    Menu management V1

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -32,4 +34,45 @@ Route::controller(UserController::class)->group(function () {
 
     Route::get('reset-password/{token}',  'showResetPasswordForm')->name('reset.password.get');
     Route::post('reset-password',  'submitResetPasswordForm')->name('reset.password.post');
+});
+
+/**
+ * | Menu Management 
+ */
+Route::controller(MenuController::class)->group(function () {
+    Route::post('save_menu', 'saveMenu');
+    Route::post('upade_menu', 'updateMenu');
+    Route::post('delete_menu', 'deleteMenu');
+
+    Route::post('_menu', 'saveMenu');
+});
+
+/**
+ * | Role Management
+ */
+Route::controller(RoleController::class)->group(function () {
+    Route::post('save_role', 'saveRole');
+    Route::post('upade_menu', 'saveMenu');
+    Route::post('delete_menu', 'saveMenu');
+
+    Route::post('_menu', 'saveMenu');
+});
+
+
+// Menu controller
+Route::controller(MenuController::class)->group(function () {
+    Route::post('user-managment/v1/crud/menu/save', 'createMenu');
+    Route::post('user-managment/v1/crud/menu/edit', 'updateMenu');
+    Route::post('user-managment/v1/crud/menu/delete', 'deleteMenu');
+    Route::post('user-managment/v1/crud/menu/get', 'getMenuById');
+    Route::post('user-managment/v1/crud/menu/list', 'menuList');
+    Route::post('user-managment/v1/crud/module/list', 'moduleList')->withoutMiddleware('auth:sanctum');
+    Route::post('user-managment/v2/crud/module/list', 'moduleListV2')->withoutMiddleware('auth:sanctum');
+    Route::post('user-managment/v1/crud/menu/list-parent-serial', 'listParentSerial');
+
+    Route::post('menu-roles/update-menu-by-role', 'updateMenuByRole');
+    Route::post('menu/get-menu-by-roles', 'getMenuByRoles');
+    Route::post('menu/by-module', 'getMenuByModuleId');
+    Route::post('sub-menu/get-children-node', 'getChildrenNode');
+    Route::post('sub-menu/tree-structure', 'getTreeStructureMenu');
 });
