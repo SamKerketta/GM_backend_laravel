71244b7d8521c2252b9880a0c2e2539b6d0d4347
mrinal9911 <mrinalk123456@gmail.com>
2025-05-16 13:01:07 +0530

    ACCESOR AND MUTATOR

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +10,31 @@ class Member extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+
+    // Capitalize Name (e.g. TANNU SHARMA)
+    public function getNameAttribute($value)
+    {
+        return strtoupper($value);
+    }
+
+    // Capitalize gender (e.g. Male, Female, Other)
+    public function getGenderAttribute($value)
+    {
+        return ucfirst($value); // or strtoupper($value) if preferred
+    }
+
+    // Format membership_start as d-m-Y
+    public function getMembershipStartAttribute($value)
+    {
+        return Carbon::parse($value)->format('d-m-Y');
+    }
+
+    // Format membership_end as d-m-Y
+    public function getMembershipEndAttribute($value)
+    {
+        return $value ? Carbon::parse($value)->format('d-m-Y') : null;
+    }
 
     /**
      * | Add Member Details
@@ -33,7 +59,7 @@ class Member extends Model
      */
     public function fetchMember()
     {
-       return Member::where('status',1)
-                        ->orderBy('name');
+        return Member::where('status', 1)
+            ->orderBy('name');
     }
 }
